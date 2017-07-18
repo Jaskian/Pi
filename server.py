f912625907d501c571dcde1f164f8eaf79912554
Bolto <bolto720@gmail.com>
2017-07-18 20:46:12 +0100

    New server file that returns non scaled values

--- server.py
+++ server.py
@@ -11,56 +11,43 @@ urls = (
 power_mgmt_1 = 0x6b
 power_mgmt_2 = 0x6c
 
-bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
-address = 0x68       # This is the address value read via the i2cdetect command
+bus = smbus.SMBus(1)
+address = 0x68
 
-
-def read_byte(adr):
-    return bus.read_byte_data(address, adr)
-
-def read_word(adr):
+def readWord(adr):
     high = bus.read_byte_data(address, adr)
     low = bus.read_byte_data(address, adr+1)
     val = (high << 8) + low
     return val
 
-def read_word_2c(adr):
-    val = read_word(adr)
+def readWord2c(adr):
+    val = readWord(adr)
     if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
+        return -((65535 - val) + 1)    
+    else :
         return val
 
 def dist(a,b):
-    return math.sqrt((a*a)+(b*b))
+    return math.sqrt((a*a) + (b*b))
 
-def get_y_rotation(x,y,z):
-    radians = math.atan2(x, dist(y,z))
+def getYRotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))    
     return -math.degrees(radians)
 
-def get_x_rotation(x,y,z):
+def getXRotation(x,y,z):
     radians = math.atan2(y, dist(x,z))
     return math.degrees(radians)
 
-
 class index:
     def GET(self):
-        accel_xout = read_word_2c(0x3b)
-        accel_yout = read_word_2c(0x3d)
-        accel_zout = read_word_2c(0x3f)
+        accelXOut = readWord2c(0x3b)
+        accelYOut = readWord2c(0x3d)
+        accelZOut = readWord2c(0x3f)
 
-        accel_xout_scaled = accel_xout / 16384.0
-        accel_yout_scaled = accel_yout / 16384.0
-        accel_zout_scaled = accel_zout / 16384.0
-
-        return str(get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))+" "+str(get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))
-
+        return str(getXRotation(accelXOut,accelYOut,accelZOut))+" "+str(getYRotation(accelXOut,accelYOut,accelZOut))
 
 if __name__ == "__main__":
-
-    # Now wake the 6050 up as it starts in sleep mode
-    bus.write_byte_data(address, power_mgmt_1, 0)
+    bus.write_byte_data(address, power_mgmt_1,0)
 
     app = web.application(urls, globals())
     app.run()
-
